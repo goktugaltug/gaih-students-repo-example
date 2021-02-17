8d88319c21c1b58a43de5464a4210e5155296dec
goktugaltug <65017373+goktugaltug@users.noreply.github.com>
2021-02-17 15:09:51 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,8 @@
-#Explain your work
+import sympy
 
-#Question 1
-for x in range(a):
-	print(a)
+for row in range(1,4):
+    list1 = []
+    for col in range(1,4):
+        num = sympy.randprime(2,100)
+        list1.append(num)       
+    print(list1)
